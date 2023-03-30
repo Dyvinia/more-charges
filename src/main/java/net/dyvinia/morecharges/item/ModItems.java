6c8b914012dd0e1b752472d29f68c4df31f6b813
Dyvine57 <dulana57@gmail.com>
2023-03-30 17:22:43 -0400

    Move things around

--- src/main/java/net/dyvinia/morecharges/item/ModItems.java
+++ src/main/java/net/dyvinia/morecharges/item/ModItems.java
@@ -1,14 +1,20 @@
 package net.dyvinia.morecharges.item;
 
 import net.dyvinia.morecharges.MoreCharges;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModItems {
-    public static final Item ENDER_CHARGE = registerItem("ender_charge", new EnderChargeItem(new FabricItemSettings().group(ModItemGroup.MORECHARGES)));
-    public static final Item AQUA_CHARGE = registerItem("aqua_charge", new AquaChargeItem(new FabricItemSettings().group(ModItemGroup.MORECHARGES)));
+    public static final ItemGroup MORECHARGES = FabricItemGroupBuilder.build(new Identifier(MoreCharges.MOD_ID, "morecharges"),
+            () -> new ItemStack(ModItems.ENDER_CHARGE));
+
+    public static final Item ENDER_CHARGE = new EnderChargeItem(new FabricItemSettings().group(MORECHARGES));
+    public static final Item AQUA_CHARGE = new AquaChargeItem(new FabricItemSettings().group(MORECHARGES));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(MoreCharges.MOD_ID, name), item);
@@ -16,6 +22,9 @@ public class ModItems {
 
     public static void registerModItems() {
         MoreCharges.LOGGER.info("Registering Mod Items for MoreCharges");
+
+        registerItem("ender_charge", ENDER_CHARGE);
+        registerItem("aqua_charge", AQUA_CHARGE);
     }
 
 }
